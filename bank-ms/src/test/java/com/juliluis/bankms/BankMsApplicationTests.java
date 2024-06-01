2cbb1a7a1a64c75390aefb04e4324d2725e88b81
jpelegrino1010 <julio.pelegrino@viseo.com>
2024-06-01 14:33:04 -0400

    properties file

--- bank-ms/src/test/java/com/juliluis/bankms/BankMsApplicationTests.java
+++ bank-ms/src/test/java/com/juliluis/bankms/BankMsApplicationTests.java
@@ -1,13 +1,13 @@
-package com.juliluis.bankms;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BankMsApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.juliluis.bankms;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BankMsApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
